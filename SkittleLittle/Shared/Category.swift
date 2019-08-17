7c19ac94bee8b92a62547901f082c60676637eb1
Luboslav  Ivanov <luboslavivanov@Luboslavs-MacBook-Pro.local>
2019-08-17 10:17:28 +0300

    Add models

---
+++ SkittleLittle/Shared/Category.swift
@@ -0,0 +1,24 @@
+//
+//  Category.swift
+//  SkittleLittle
+//
+//  Created by Luboslav  Ivanov on 17.08.19.
+//  Copyright © 2019 ApplePie. All rights reserved.
+//
+
+import UIKit
+import Foundation
+
+class Category {
+    var name: String
+    var image: UIImage // Fix later
+    var defaultBudgetType: 
+    var transactionType: TransactionType
+    
+    init(name: String, image: UIImage, defaultBudgetType: BugetType, transactionType: TransactionType) {
+        self.name = name
+        self.image = image
+        self.defaultBudgetType = defaultBudgetType
+        self.transactionType = transactionType
+    }
+}
---
+++ SkittleLittle/Shared/PersonalSettings.swift
@@ -0,0 +1,16 @@
+//
+//  PersonalSettings.swift
+//  SkittleLittle
+//
+//  Created by Luboslav  Ivanov on 17.08.19.
+//  Copyright © 2019 ApplePie. All rights reserved.
+//
+
+import Foundation
+
+struct PersonalSettings {
+    var initBalance: Double
+    var currentBalance: Double
+    var currency: String
+//    var period: PeriodType
+}
